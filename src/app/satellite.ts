e4167e8d3abf8bb16c1a8c5c1286132d75bbe2f9
bstoverink <bstoverink@gmail.com>
2021-04-06 18:06:51 -0500

    Just search left to finish

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,14 +1,21 @@
+import { style } from "@angular/animations";
+
 export class Satellite {
 name: string;
 orbitType: string;
 type: string;
 operational: boolean;
 launchDate: string;
+shouldShowWarning(){
+
+}
 constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean){
     this.name= name;
     this.type= type;
     this.launchDate= launchDate;
     this.orbitType= orbitType;
     this.operational= operational;
+ 
     }
+
 }
